577397159c0c8d4ca6aeb3ed977559236f868839
Leewel Karani <leewelkarani@gmail.com>
2020-01-02 21:07:20 +0300

    Add delete movie controller

--- server/controllers/movie_ctrl.js
+++ server/controllers/movie_ctrl.js
@@ -75,3 +75,17 @@ updateMovie = async (req, res) => {
     });
   }
 };
+
+deleteMovie = async (req, res) => {
+  await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!movie) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+
+    return res.status(200).json({ success: true, data: movie });
+  }).catch(err => console.log(err));
+};
