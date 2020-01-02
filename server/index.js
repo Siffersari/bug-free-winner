cb2b34c98e5fcf503f0014ca36c0d34486d839d8
Leewel Karani <leewelkarani@gmail.com>
2020-01-02 21:46:14 +0300

    configure the router to the app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const db = require("./db");
+const movieRouter = require("./routes/movie-router");
 
 const app = express();
 const apiPort = 3000;
@@ -16,5 +17,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error: "));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api", movieRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
