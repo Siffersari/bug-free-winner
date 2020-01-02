cbb3b71647e70c5394fc88e98e62ae465abaea0b
Leewel Karani <leewelkarani@gmail.com>
2020-01-02 21:10:58 +0300

    add fetch movie list controller

--- server/controllers/movie_ctrl.js
+++ server/controllers/movie_ctrl.js
@@ -13,10 +13,7 @@ createMovie = (req, res) => {
   const movie = new Movie(body);
 
   if (!movie) {
-    return res.status(400).json({
-      success: false,
-      error: err
-    });
+    return res.status(400).json({ success: false, error: err });
   }
 
   movie
@@ -42,38 +39,36 @@ updateMovie = async (req, res) => {
   if (!body) {
     return res.status(400).json({
       success: false,
-      error: "You must provide a Movie to update"
-    });
-
-    Movie.findOne({ _id: req.params.id }, (err, movie) => {
-      if (err) {
-        return res.status(404).json({
-          err,
-          message: "Movie not found"
-        });
-      }
-
-      movie.name = body.name;
-      movie.time = body.time;
-      movie.rating = body.rating;
-
-      movie
-        .save()
-        .then(() => {
-          return res.status(200).json({
-            success: true,
-            id: movie._id,
-            message: "Movie updated!"
-          });
-        })
-        .catch(error => {
-          return res.status(404).json({
-            error,
-            message: "Movie not updated!"
-          });
-        });
+      error: "You must provide a body to update"
     });
   }
+
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Movie not found!"
+      });
+    }
+    movie.name = body.name;
+    movie.time = body.time;
+    movie.rating = body.rating;
+    movie
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: movie._id,
+          message: "Movie updated!"
+        });
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: "Movie not updated!"
+        });
+      });
+  });
 };
 
 deleteMovie = async (req, res) => {
@@ -101,4 +96,24 @@ getMovieById = async (req, res) => {
     }
     return res.status(200).json({ success: true, data: movie });
   }).catch(err => console.log(err));
+};
+
+getMovies = async (req, res) => {
+  await Movie.find({}, (err, movies) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!movies.length) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+    return res.status(200).json({ success: true, data: movies });
+  }).catch(err => console.log(err));
+};
+
+module.exports = {
+  createMovie,
+  updateMovie,
+  deleteMovie,
+  getMovies,
+  getMovieById
 };
