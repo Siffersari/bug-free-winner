457accc2c72861c9b537efd4d6b1e215b8cd389d
Leewel Karani <leewelkarani@gmail.com>
2020-01-02 21:09:50 +0300

    Add fetch One movie controller

--- server/controllers/movie_ctrl.js
+++ server/controllers/movie_ctrl.js
@@ -89,3 +89,16 @@ deleteMovie = async (req, res) => {
     return res.status(200).json({ success: true, data: movie });
   }).catch(err => console.log(err));
 };
+
+getMovieById = async (req, res) => {
+  await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!movie) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+    return res.status(200).json({ success: true, data: movie });
+  }).catch(err => console.log(err));
+};
