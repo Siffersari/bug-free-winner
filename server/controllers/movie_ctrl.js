de395c8edacba56d9b326745ec499bdd91957195
Leewel Karani <leewelkarani@gmail.com>
2020-01-02 20:52:05 +0300

    Add movie controller

---
+++ server/controllers/movie_ctrl.js
@@ -0,0 +1,37 @@
+const Movie = require("../models/movie-model");
+
+createMovie = (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a movie"
+    });
+  }
+
+  const movie = new Movie(body);
+
+  if (!movie) {
+    return res.status(400).json({
+      success: false,
+      error: err
+    });
+  }
+
+  movie
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: movie._id,
+        message: "Movie created!"
+      });
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error,
+        message: "Movie not created!"
+      });
+    });
+};
