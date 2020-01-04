5319eb299e060fd7480e1d59afaa21bef1edaf22
Leewel Karani <leewelkarani@gmail.com>
2020-01-04 23:55:59 +0300

    Add movie listing page

---
+++ client/src/pages/MoviesList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MoviesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>In this page you'll see the list of movies</p>
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
